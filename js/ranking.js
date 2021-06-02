a8c835aac5ee833d712024fa9a8c64f5c17cc940
Victor <vicwalero@gmail.com>
2021-06-02 21:33:42 +0200

    Update ranking.js

--- js/ranking.js
+++ js/ranking.js
@@ -2,21 +2,21 @@ function rankingFIFA(link) {
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("torneos").innerHTML = this.responseText;
+            document.getElementById("tabla").innerHTML = this.responseText;
         }
     };
     alert("aaa");
     http.open("GET", link + "php/rankingTorneos.php", true);
     http.send(null);
-    function rankingLOL(link) {
-        var http = new XMLHttpRequest();
-        http.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("torneos").innerHTML = '<tr class="top_tabla"><td>Puesto</td><td>Nombre de usuario</td><td>Puntuación</td></tr>' + this.responseText;
-            }
-        };
-        alert("bbb");
-        http.open("GET", link + "php/rankingTorneos.php", true);
-        http.send(null);
-    }
+}
+function rankingLOL(link) {
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("tabla").innerHTML = '<tr class="top_tabla"><td>Puesto</td><td>Nombre de usuario</td><td>Puntuación</td></tr>' + this.responseText;
+        }
+    };
+    alert("hola");
+    http.open("GET", link + "php/rankingTorneos.php", true);
+    http.send(null);
 }
